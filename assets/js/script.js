2cc3d58d8a8fcd653d9e4f5ea99954ac1e47041d
Gregory Casanova <gregory.casanova@alumnos.usm.cl>
2021-08-17 15:28:31 -0400

    Desafío terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,59 @@
 $(() => {
-  if ($(window).width() < 992) {
-    $('#nav-menu, #nav-button i:last-child').hide();
-    $('#nav-button').change(function () {
-      $('#nav-menu').slideToggle();
-      $('#nav-button i').toggle();
-    });
+
+  let displayNavMobile = () => $('#nav-menu, #nav-button i:last-child').hide();
+  let displayNavLaptop = () => $('#nav-menu').show();
+
+  let toggleNavbar = () => {
+    $('#nav-menu').slideToggle();
+    $('#nav-button i').toggle();
+  };
+
+  let setScrollMobile = () => {
     $('.nav-menu__item').on('tap click', function (event) {
       event.preventDefault();
-      $('#nav-menu').slideToggle();
-      $('#nav-button i').toggle();
-      $('html, body').animate({scrollTop: $(this.hash).offset().top - $('.grid-item-1').height()}, 2000, 'swing', function() {});
+      toggleNavbar();
+      let $hash = $(this.hash);
+      $('html, body').animate({scrollTop: $hash.offset().top - ($('.grid-item-1').height()*0.3225)}, 1000, 'linear', function () {
+        $hash.focus();
+      });
     });
-  } else {
+  };
+
+  let setScrollLaptop = () => {
     $('.nav-menu__item').on('click', function (event) {
       event.preventDefault();
-      $('html, body').animate({scrollTop: $(this.hash).offset().top}, 2000, 'swing', function() {});
+      let $hash = $(this.hash);
+      $('html, body').stop().animate({scrollTop: $hash.offset().top}, 1000, 'linear', function() {
+        $hash.focus();
+      });
     });
-  }
+  };
+
+  let unsetScroll = () => {
+    $('.nav-menu__item').off();
+  };
+
+  if ($(window).width() < 992) {
+    displayNavMobile();
+    setScrollMobile();
+  } else {
+    setScrollLaptop();
+  };
+
+  $('#nav-button').change(function () {
+    toggleNavbar();
+  });
+
+  $(window).resize(function () {
+    if ($(window).width() < 992) {
+      unsetScroll();
+      displayNavMobile();
+      setScrollMobile();
+    } else {
+      unsetScroll();
+      displayNavLaptop();
+      setScrollLaptop();
+    }
+  });
+
 });
